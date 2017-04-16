0ad1907cc42a8bd4c4d170d2b3f17038c89234a1
deswyn <deswync@gmail.com>
2017-04-16 15:18:34 -0400

    Created New Test Level and Screen Effects

---
+++ Captstone Game/Assets/Scripts/ScreenEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class ScreenEffects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.GetComponent<UnityStandardAssets.ImageEffects.Blur>().enabled = true;
+        //Camera.main.GetComponent<UnityStandardAssets.ImageEffects.Blur>().iterations = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Mushroom")
+        {
+            Camera.main.GetComponent<UnityStandardAssets.ImageEffects.Blur>().iterations = 10;
+        }
+    }
+}
