35144570dbccc4c827c474561db2f17d0e09c0ad
deswyn <deswync@gmail.com>
2017-05-02 23:02:02 -0400

    Changed damage room

---
+++ Capstone Milestone 3 Test Effect Level/Test Effects/Assets/Scripts/ParticleDamage.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDamage : MonoBehaviour {
+
+    bool promptKey = false;
+
+    public bool isDamaging;
+    public float damageAmount = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            promptKey = true;
+        }
+    }
+
+
+    //stops particle system
+    ParticleSystem sparks
+    {
+        get
+        {
+            if (_CachedSparks == null)
+                _CachedSparks = GetComponent<ParticleSystem>();
+            return _CachedSparks;
+        }
+    }
+
+    private ParticleSystem _CachedSparks;
+    public bool includeChildren = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (promptKey == true)
+        {
+
+            sparks.Stop(includeChildren);
+            damageAmount = 0;
+
+        }
+
+        //other.SendMessage((isDamaging) ? "TakeDamage" : "HealDamage", Time.deltaTime * damageAmount);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        other.SendMessage((isDamaging) ? "TakeDamage" : "HealDamage", Time.deltaTime * damageAmount);
+    }
+}
---
+++ Capstone Milestone 3 Test Effect Level/Test Effects/Assets/Scripts/TriggerDamageRoom.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDamageRoom : MonoBehaviour {
+
+    bool promptKey = false;
+
+    public bool isDamaging;
+    public float damageAmount = 10;
+
+    ParticleSystem vent1;
+    ParticleSystem vent2;
+    ParticleSystem vent3;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            promptKey = true;
+        }
+
+    }
+
+    //stops particle system
+    ParticleSystem sparks
+    {
+        get
+        {
+            if (_CachedSparks == null)
+                _CachedSparks = GetComponent<ParticleSystem>();
+            
+           
+            return _CachedSparks;
+        }
+    }
+
+  
+    ParticleSystem _CachedSparks;
+    public bool includeChildren = true;
+
+  
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //player takes damage as long as they remain in damage zone
+
+            other.SendMessage((isDamaging) ? "TakeDamage" : "HealDamage", Time.deltaTime * damageAmount);
+
+            if(promptKey == true)
+            {
+                sparks.Stop(includeChildren);
+            }
+        }
+
+        
+    }
+
+}
