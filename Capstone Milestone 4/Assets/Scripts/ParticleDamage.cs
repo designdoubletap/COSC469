28617fb05b9709b2cfe41b2a2d89bd45a15aca8a
deswyn <deswync@gmail.com>
2017-05-04 08:30:50 -0400

    Milestone 4 Reuploaded

---
+++ Capstone Milestone 4/Assets/Scripts/ParticleDamage.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDamage : MonoBehaviour {
+
+    bool promptKey = false;
+
+    public bool isDamaging = true;
+    public float damageAmount = 10;
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            promptKey = true;
+        }
+    }
+
+
+    //stops particle system
+    ParticleSystem sparks
+    {
+        get
+        {
+            if (_CachedSparks == null)
+                _CachedSparks = GetComponent<ParticleSystem>();
+            return _CachedSparks;
+        }
+    }
+
+    private ParticleSystem _CachedSparks;
+    public bool includeChildren = true;
+
+    void OnTriggerStay(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        if (other.tag == "Player" && (Input.GetKeyDown(KeyCode.E)))
+        {
+
+            sparks.Stop(includeChildren);
+            damageAmount = 0;
+            audio.Stop();
+            //AudioSource.Stop();
+            
+
+        }
+
+        //other.SendMessage((isDamaging) ? "TakeDamage" : "TakeDamage", Time.deltaTime * damageAmount);
+    }
+
+   
+}
---
+++ Capstone Milestone 4/Assets/Scripts/PickupObjects.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObjects : MonoBehaviour {
+
+    public Transform player;
+    public Transform pCam;
+
+    public float playerMass;
+    public float objMass;
+
+    bool inRange = false;
+    bool pickedUp = false;
+    bool wallTouch = false;
+
+    // Use this for initialization
+    void Start () {
+        
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerMass = player.GetComponent<Rigidbody>().mass;
+        objMass = this.GetComponent<Rigidbody>().mass;
+
+        float playerRange = Vector3.Distance(gameObject.transform.position, player.position);
+
+        //is player in range?
+        if (playerRange <= 1f)
+        {
+            inRange = true;
+        }
+        else { inRange = false; }
+
+        //player in range and is pressing interact button
+        if (inRange == true && Input.GetButtonDown("Interact") && (playerMass > objMass))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = pCam;
+            pickedUp = true;
+        }
+
+        if (pickedUp == true)
+        {
+
+            GetComponent<Rigidbody>().useGravity = true;
+
+            //drop object if player touches wall
+            if (wallTouch == true)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+
+                transform.parent = null;
+                pickedUp = false;
+                wallTouch = false;
+
+            }
+
+            //throw
+            if (Input.GetMouseButtonDown(0) == true)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+
+                transform.parent = null;
+                pickedUp = false;
+                GetComponent<Rigidbody>().AddForce(pCam.forward * playerMass);
+
+
+            }
+            //drop
+            else if (Input.GetMouseButton(1))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                pickedUp = false;
+
+            }
+        }
+
+         
+    }
+}
