91f41e8cff53994a00a8cd8a24d2f53e68305eec
deswyn <deswync@gmail.com>
2017-11-04 20:07:23 -0400

    UI Changes
    Added Loading screen

---
+++ Capstone 2 Game/Where am I/Assets/Scripts/LoadingScreenButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreenButtonManager : MonoBehaviour {
+
+    public GameObject proceedGO;
+    public Button proceed;
+
+	// Use this for initialization
+	
+
+    void Start()
+    {
+        proceed.onClick.AddListener(delegate { Proceed(); });
+
+        proceedGO.SetActive(false);
+
+        StartCoroutine(Wait());
+
+        
+        
+    }
+
+    public void Proceed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(15f);
+        proceedGO.SetActive(true);
+    }
+	
+	
+}
